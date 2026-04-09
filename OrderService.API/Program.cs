e73892a213258eebff7fc848a89bfbc3538fdb9e
ninohaegglund <ninomh99@gmail.com>
2026-04-09 15:57:25 +0200

    Store final changes for step 'Add Swagger to OrderService.API'

--- OrderService.API/Program.cs
+++ OrderService.API/Program.cs
@@ -7,11 +7,9 @@ using OrderServiceImplementation = OrderService.Api.Services.OrderService;
         
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<OrderDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -21,10 +19,10 @@ builder.Services.AddScoped<IOrderService, OrderServiceImplementation>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
