e03ac3b1a13a6cebc2021224fd075a5f06853746
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2026-04-10 10:40:21 +0200

    Add EF Core, Swagger, and SQL Server config to API project
    Replaced OpenAPI with Swagger for API docs. Added Entity Framework Core packages and configured SQL Server connection in appsettings.json. Registered repositories and services for DI. Added folders for Data, Interfaces, DTOs, and Models.

--- CatalogService.API/Program.cs
+++ CatalogService.API/Program.cs
@@ -1,17 +1,29 @@
+using CatalogService.Api.Data;
+using CatalogService.Api.Interfaces;
+using CatalogService.Api.Repositories;
+using CatalogService.Api.Services;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CatalogDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
+builder.Services.AddScoped<IProductService, ProductService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
