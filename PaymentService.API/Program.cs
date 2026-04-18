1ccd80290603258784d55239afb6a111e9219f36
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2026-04-18 13:24:00 +0200

    Register IPaymentService in DI container
    Added IPaymentService with PaymentService implementation to the dependency injection container using AddScoped, enabling its injection throughout the application.

--- PaymentService.API/Program.cs
+++ PaymentService.API/Program.cs
@@ -1,15 +1,18 @@
 using Microsoft.EntityFrameworkCore;
 using PaymentService.Api.Data;
+using PaymentService.Api.Interfaces;
+using PaymentService.Api.Repositories;
+using PaymentServiceImplementation = PaymentService.Api.Services.PaymentService;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-
 builder.Services.AddDbContext<PaymentDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<IPaymentRepository, PaymentRepository>();
+builder.Services.AddScoped<IPaymentService, PaymentServiceImplementation>();
 
 var app = builder.Build();
 
