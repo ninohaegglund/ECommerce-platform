65dfe79f31e00402e88a5a315d3fa2a7c07c82fe
ninohaegglund <ninomh99@gmail.com>
2026-05-07 10:48:29 +0200

    fix dto property

--- CatalogService.API/DTOs/Products/ProductImageRequestDto.cs
+++ CatalogService.API/DTOs/Products/ProductImageRequestDto.cs
@@ -1,4 +1,6 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 
 namespace CatalogService.Api.DTOs.Products;
 
@@ -12,5 +14,18 @@ public class ProductImageRequestDto
     [MaxLength(300)]
     public string AltText { get; set; } = string.Empty;
     public int SortOrder { get; set; }
+    public bool IsPrimary { get; set; }
+}
+
+public class ProductImageUploadRequestDto
+{
+    [Required]
+    public IFormFile Image { get; set; } = default!;
+
+    [MaxLength(300)]
+    public string? AltText { get; set; }
+
+    public int SortOrder { get; set; }
+
     public bool IsPrimary { get; set; }
 }
