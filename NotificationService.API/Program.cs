7484fb2294fe1ca27e6a04ef96e7143b57f62a88
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2026-05-27 16:47:02 +0200

    Allow notification HTTP in development

--- NotificationService.API/Program.cs
+++ NotificationService.API/Program.cs
@@ -76,7 +76,11 @@ using (var scope = app.Services.CreateScope())
     await dbContext.Database.MigrateAsync();
 }
 
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
+
 app.UseCors("FrontendPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
