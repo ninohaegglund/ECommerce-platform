4a2781d56eb96aa6a23d9081a32e6c3476d6cbff
Max Jacobsson <Max.Jacobsson1999@hotmail.com>
2026-06-08 21:51:49 +0200

    Add auth token field migrations

---
+++ IdentityService.API/Migrations/20260608090000_AddEmailVerificationToUsers.cs
@@ -0,0 +1,61 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityService.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddEmailVerificationToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "EmailConfirmed",
+                table: "Users",
+                type: "bit",
+                nullable: false,
+                defaultValue: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmailConfirmedAtUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "EmailVerificationTokenHash",
+                table: "Users",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EmailVerificationTokenExpiresAtUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmed",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EmailConfirmedAtUtc",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationTokenHash",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "EmailVerificationTokenExpiresAtUtc",
+                table: "Users");
+        }
+    }
+}
---
+++ IdentityService.API/Migrations/20260608093000_AddPasswordResetToUsers.cs
@@ -0,0 +1,50 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace IdentityService.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPasswordResetToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetRequestedAtUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "PasswordResetTokenExpiresAtUtc",
+                table: "Users",
+                type: "datetime2",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PasswordResetTokenHash",
+                table: "Users",
+                type: "nvarchar(128)",
+                maxLength: 128,
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordResetRequestedAtUtc",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetTokenExpiresAtUtc",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "PasswordResetTokenHash",
+                table: "Users");
+        }
+    }
+}
